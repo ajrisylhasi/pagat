aea732ecceb6080ff33ea7514df957f273d6b237
Ajri Sylhasi <ajrisylhasi@gmail.com>
2020-10-09 14:43:17 +0200

    kontratat spec

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -116,7 +116,7 @@ module UsersHelper
 			end
 			if user == nil
 				user = User.create(idnum: "#{row[0].strip}", name: "#{row[1]}".gsub(" ","") + " " + "#{row[2]}".gsub(" ",""), personal_number: "#{row[3].to_i}".gsub(" ",""), email: "#{row[4]}".gsub(" ",""), salary: row[5].to_f, contract: row[6].to_i)
-				SpecificContract.create(user: @user)
+				SpecificContract.create(user: user)
 				row[7] = "" if row[7] == nil
 				ditet = row[7].split(",")
 				ditet = [] if ditet == nil
