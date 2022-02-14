9cbee3c880faa2dc8682f2eda9e166ee0db7a30d
ajrisylhasi <ajrisylhasi@gmail.com>
2022-02-14 21:45:47 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,14 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-    # :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'smtp.gmail.com',
-    # :user_name => "humanresources@albinadyla.com",
-    # :password => "qtjkkzsrhkjnsdly",
-    :authentication => :login,
-    :enable_starttls_auto => true
-  } 
+  config.action_mailer.default :charset => "utf-8"]
 end
